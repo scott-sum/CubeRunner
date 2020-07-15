1d833ab33c4e113e18fb8514f09e0feb17483a14
Scott Sum <scottsum@outlook.com>
2020-07-14 23:18:06 -0400

    first commit

---
+++ CubeRunner/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement; //reference to our PlayerMovement script
+    
+    //This function runs when the player collides with an obstacle
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false; //disable player's movement (stop)
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+}
---
+++ CubeRunner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    //Whenever doing things with physics, do it inside FixedUpdate
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Time.deltaTime = amount of seconds since the computer drew the last frame
+
+        // pressing 'd' will add force to the right
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        // pressing 'a' will add force to the left
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
